a1084d2187635f577d25e4bba79abf336b0a5f7b
nguyenhoangthienchi <jayesd3v@gmail.com>
2024-05-28 13:34:40 +0700

    add some test

---
+++ Object Repository/Page_Knh tin Game ln nht, th vin Game y  nht/a_GAME MOBILE.rs
@@ -0,0 +1,130 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>a_GAME MOBILE</name>
+   <tag></tag>
+   <elementGuidId>c8f8fb9a-7812-4955-866c-78b87f5e8de9</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@id='admWrapsite']/div/div/div/div[2]/ul/li[2]/a</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>a[title=&quot;GAME MOBILE&quot;]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorCollection>
+      <entry>
+         <key>SMART_LOCATOR</key>
+         <value>internal:role=link[name=&quot;GAME MOBILE&quot;i] >> nth=0</value>
+      </entry>
+   </smartLocatorCollection>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>a</value>
+      <webElementGuid>34176ec4-158f-4188-814f-56e6a3ea2724</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>title</name>
+      <type>Main</type>
+      <value>GAME MOBILE</value>
+      <webElementGuid>50149d37-df88-4b63-a188-99fa3434e2d4</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>href</name>
+      <type>Main</type>
+      <value>/mobile-social.chn</value>
+      <webElementGuid>69bb4c3c-2d55-4048-add2-18808c7fd95c</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>GAME MOBILE</value>
+      <webElementGuid>3cf41f1c-0d32-4f11-8e2d-dd0e1a1cf28a</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;admWrapsite&quot;)/div[@class=&quot;wp1000 wfull&quot;]/div[@class=&quot;wp1000&quot;]/div[@class=&quot;header&quot;]/div[@class=&quot;menu&quot;]/ul[1]/li[@class=&quot;normal&quot;]/a[1]</value>
+      <webElementGuid>db8489e1-e347-4187-8aa6-fde8f37a6265</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//div[@id='admWrapsite']/div/div/div/div[2]/ul/li[2]/a</value>
+      <webElementGuid>2812dfed-21cd-4168-805a-543a4774584c</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:link</name>
+      <type>Main</type>
+      <value>//a[contains(text(),'GAME MOBILE')]</value>
+      <webElementGuid>6b465f19-6af7-4b8c-9911-cfc43b188520</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='eSPORTS'])[1]/preceding::a[1]</value>
+      <webElementGuid>1500104e-5f08-4601-8c0b-5988c9c7c2d3</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='KHÁM PHÁ'])[1]/preceding::a[2]</value>
+      <webElementGuid>32e20dec-af4d-423c-a5dd-413734c890a2</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>//*/text()[normalize-space(.)='GAME MOBILE']/parent::*</value>
+      <webElementGuid>c0c2c961-b8b7-4f81-a985-c76d80333cbb</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:href</name>
+      <type>Main</type>
+      <value>//a[contains(@href, '/mobile-social.chn')]</value>
+      <webElementGuid>0fac18e0-a319-4f09-8abd-96ab004da2bf</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//li[2]/a</value>
+      <webElementGuid>365f7543-33da-4bef-8e97-c45788975583</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//a[@title = 'GAME MOBILE' and @href = '/mobile-social.chn' and (text() = 'GAME MOBILE' or . = 'GAME MOBILE')]</value>
+      <webElementGuid>c34ea71b-a6d3-4f2d-bfad-f889baa6eec9</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
