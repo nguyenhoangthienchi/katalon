ed6fd080858c95fcb3f87ea0d9c00e14b71c08f9
nguyenhoangthienchi <jayesd3v@gmail.com>
2024-05-29 23:24:49 +0700

    test

---
+++ Test Suites/desktop.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>desktop</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>93555dfa-ad85-46ca-9320-737a34edd6ca</testSuiteGuid>
+   <testCaseLink>
+      <guid>8c81be9a-f394-4fed-974b-365bd3668035</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/desktop/desktops</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
