c0c2fe415c34c3951228312af31a8c1acf829e9a
nguyenhoangthienchi <jayesd3v@gmail.com>
2024-05-31 00:20:53 +0700

    add object for visual testing

---
+++ Object Repository/Page_The Fastest Web Page on the Internet/a_Learn to code more useful websites than this one.rs
@@ -0,0 +1,114 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>a_Learn to code more useful websites than this one</name>
+   <tag></tag>
+   <elementGuidId>910d81ef-cd7e-4230-9644-feb1471b609f</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//a[contains(text(),'Learn to code more useful websites than this one')]</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>a</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorCollection>
+      <entry>
+         <key>SMART_LOCATOR</key>
+         <value>internal:role=link[name=&quot;Learn to code more useful websites than this one&quot;i]</value>
+      </entry>
+   </smartLocatorCollection>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>a</value>
+      <webElementGuid>506fdcc4-c50f-4f61-a931-78da84a17694</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>href</name>
+      <type>Main</type>
+      <value>https://www.freecodecamp.org</value>
+      <webElementGuid>d9090dfb-57eb-4ef4-bb36-1e6f1f82b91f</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Learn to code more useful websites than this one</value>
+      <webElementGuid>1a236458-4ce9-426d-ab84-f6ccef2aa2d2</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>/html[1]/body[1]/p[4]/a[1]</value>
+      <webElementGuid>9faa81af-3c87-4419-b783-def27e433ef4</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:link</name>
+      <type>Main</type>
+      <value>//a[contains(text(),'Learn to code more useful websites than this one')]</value>
+      <webElementGuid>75feb3dd-63a2-4d40-aa0d-68e4aaeb3095</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='This is the fastest web page on the internet.'])[1]/following::a[1]</value>
+      <webElementGuid>32748eb7-c4b0-4b43-908c-f5ef91901447</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='/html[1]/body[1]/p[4]/a[1]'])[1]/preceding::a[1]</value>
+      <webElementGuid>19408959-5296-4c3d-884c-c9d69fddd9f4</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>//*/text()[normalize-space(.)='Learn to code more useful websites than this one']/parent::*</value>
+      <webElementGuid>567ad5da-ce59-43cc-92a6-fba7e3fef737</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:href</name>
+      <type>Main</type>
+      <value>//a[contains(@href, 'https://www.freecodecamp.org')]</value>
+      <webElementGuid>fd75bd68-5351-4066-bf53-063313ead88a</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//a</value>
+      <webElementGuid>3e4ad622-8563-42de-b2a9-c06918c9a3cc</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//a[@href = 'https://www.freecodecamp.org' and (text() = 'Learn to code more useful websites than this one' or . = 'Learn to code more useful websites than this one')]</value>
+      <webElementGuid>ae161cd8-50b1-4beb-9fbf-6ae713710701</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
---
+++ Object Repository/Page_Thegioididong.com - in thoi, Laptop, P_d6f42f/a_Game App.rs
@@ -0,0 +1,130 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>a_Game App</name>
+   <tag></tag>
+   <elementGuidId>cb122fb3-58fb-4250-a9f6-0ece7ad794c1</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//a[contains(text(),'Game App')]</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorCollection>
+      <entry>
+         <key>SMART_LOCATOR</key>
+         <value>internal:role=link[name=&quot;Game App&quot;i]</value>
+      </entry>
+   </smartLocatorCollection>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>a</value>
+      <webElementGuid>2b0b69c8-df9a-48cc-8eb6-32c90437348c</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>href</name>
+      <type>Main</type>
+      <value>/game-app</value>
+      <webElementGuid>b1adc326-7d8a-4f87-80d9-839f5e896668</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Game App</value>
+      <webElementGuid>ff05b54d-d9ba-41d9-88ab-2c7a4fa67dba</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>/html[1]/body[@class=&quot;&quot;]/header[@class=&quot;header&quot;]/div[@class=&quot;header__top&quot;]/div[1]/div[@class=&quot;header__listtop&quot;]/div[@class=&quot;divitem&quot;]/a[1]</value>
+      <webElementGuid>3f54448c-7c75-4b10-ab5e-6a69cd09048d</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:link</name>
+      <type>Main</type>
+      <value>//a[contains(text(),'Game App')]</value>
+      <webElementGuid>a31f4073-6541-40fa-827e-79952e3c08f0</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Hỏi Đáp'])[1]/following::a[1]</value>
+      <webElementGuid>97d228e0-f472-47bf-bc0e-978cfe1efd58</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Giỏ hàng'])[1]/following::a[3]</value>
+      <webElementGuid>ffb18c68-e0c4-4818-8a7f-814d7e94f617</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Điện thoại'])[1]/preceding::a[1]</value>
+      <webElementGuid>00142cee-52ce-4831-8be8-f752aabeb8bc</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Laptop'])[1]/preceding::a[2]</value>
+      <webElementGuid>95e053aa-1ba9-43c2-9abf-ec9dc00d587d</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>//*/text()[normalize-space(.)='Game App']/parent::*</value>
+      <webElementGuid>a967ce6f-34c1-4908-8aa7-76bbcc5dc104</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:href</name>
+      <type>Main</type>
+      <value>//a[contains(@href, '/game-app')]</value>
+      <webElementGuid>83a3a8c4-981c-44ff-b159-2f72cc068875</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div[5]/a</value>
+      <webElementGuid>048647a1-d2ad-4627-85d6-69579f3a9cab</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//a[@href = '/game-app' and (text() = 'Game App' or . = 'Game App')]</value>
+      <webElementGuid>a4f6d642-71f0-4120-8d85-39e19356f0f7</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
